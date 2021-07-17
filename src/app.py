992907f21a3259d82bf94d054fae9c8d3446518c
Misaal <makstereb@gmail.com>
2021-07-17 09:42:41 +0300

    Add error pages

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, redirect, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,15 +9,38 @@ def get_theme():
     return request.cookies.get('theme') or 'dark'
 
 
+@app.before_request
+def before_request():
+    if request.path != '/' and request.path.endswith('/'):  # remove trailing slash
+        return redirect(request.path.removesuffix('/'))
+
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
 
 
+@app.errorhandler(404)
+def page_not_found(_):
+    return render_template('error.html', theme=get_theme(), error_code=404,
+                           error_message='Тут ничего нет. Ну или я хочу чтоб вы так думали.')
+
+
+@app.errorhandler(500)
+def internal_server_error(_):
+    return render_template('error.html', theme=get_theme(), error_code=500,
+                           error_message='Я накосячил. Но виноваты всё равно вы.')
+
+
 @app.route('/')
 def index():
-    return render_template('base.html', theme=get_theme())
+    return render_template('index.html', theme=get_theme())
+
+
+@app.route('/error')
+def error():
+    raise Exception
 
 
 if __name__ == '__main__':
